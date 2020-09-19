c8db735dec90972a4fbd4055af92f89be9b2df99
Scott Hoisington <hoyzington@gmail.com>
2020-09-19 05:52:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const multiply = x;
-const random = Math.ceil(Math.random() * 10);
-const mod = 16;
-const max = Math.max(20);
+// const multiply = x;
+// const random = Math.ceil(Math.random() * 10);
+// const mod = 16;
+// const max = Math.max(20);
+
+const var1 = 2
+const var2 = 31
+const multiply = var1 * var2
+const random = Math.floor( Math.random() * 1000 ) + 1
+const mod = 16 % 6
+const max = Math.max( 5, 10, 15, 20 )
